198d78c8e9190ffd63b19604349e1db4d71869fb
taek105 <ht5172@gmail.com>
2024-11-06 15:47:18 +0900

    feat(Validate): add Validate
    파일 형식 체크를 하는 parseProduct, parsePromotion 함수 추가

--- src/main/java/store/constant/ErrorMessage.java
+++ src/main/java/store/constant/ErrorMessage.java
@@ -6,7 +6,8 @@ public enum ErrorMessage {
     NOT_EXISTENCE_PROMOTION("[ERROR] 존재하지 않는 프로모션입니다. 다시 입력해 주세요."),
     EXCEED_QUANTITY("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
     FAULT_INPUT("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요."),
-    NOT_VALID_FILEROUTE("[ERROR] 파일 경로가 잘못됐습니다. Promotions 참고하세용");
+    NOT_VALID_FILE_ROUTE("[ERROR] 파일 경로가 잘못되었습니다."),
+    NOT_VALID_FILE_FORMAT("[ERROR] 파일 형식이 잘못되었습니다.");
 
     private final String message;
 
