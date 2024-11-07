f13eb808c41707c626ab83c377bdab30dfd2410b
taek105 <ht5172@gmail.com>
2024-11-07 21:14:23 +0900

    test(PromotionService): add PromotionService test
    Promotion 로직 테스트를 추가했습니다
    - 손님이 놓친 프로모션 물품을 제공할 지 물어보는 테스트
    - 프로모션 재고가 부족할 때 일부 수량을 혜택 없이 살 지 물어보는 테스트

--- src/test/java/store/service/ConvenienceServiceTest.java
+++ src/test/java/store/service/ConvenienceServiceTest.java
@@ -6,7 +6,6 @@ import store.domain.Products;
 import store.domain.Promotions;
 
 import java.io.IOException;
-import java.io.ByteArrayInputStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -23,111 +22,22 @@ public class ConvenienceServiceTest {
 
         Product found = products.getProduct(productName, true);
         int q = found.getQuantity();
+        System.out.println(found.getName());
 
-        assertEquals(convenienceService.purchase(productName, amount), found.getPrice()*2);
+        assertEquals(found.getPrice()*2, convenienceService.purchase(productName, amount));
         assertEquals(q- amount, found.getQuantity());
     }
 
     @Test
-    void 투쁠투_3개구매_Y_PROMOTE_테스트() {
-        String productName = "효택도시락";
+    void 투쁠원_6개구매_테스트() {
+        String productName = "탄산수";
         int amount = 3;
 
         Product found = products.getProduct(productName, true);
         int q = found.getQuantity();
+        System.out.println(found.getName());
 
-        assertEquals(convenienceService.purchase(productName, amount), found.getPrice()*2);
-        assertEquals(q-4, found.getQuantity());
-    }
-
-    @Test
-    void 투쁠투_3개구매_N_PROMOTE_테스트() {
-        String productName = "효택도시락";
-        int amount = 3;
-
-        Product found = products.getProduct(productName, true);
-        int q = found.getQuantity();
-
-        assertEquals(convenienceService.purchase(productName, amount), found.getPrice()*2);
-        assertEquals(q- amount, found.getQuantity());
-    }
-
-    @Test
-    void 투쁠투_5개구매_테스트() {
-        String productName = "효택도시락";
-        int amount = 5;
-
-        Product found = products.getProduct(productName, true);
-        int q = found.getQuantity();
-
-        int realBoughtQuantity = 0;
-        int i = 0;
-        while (i < amount) {
-            i ++;
-            realBoughtQuantity ++;
-            if ( i %  found.getPromotion().getBuy() == 0) {
-                i += found.getPromotion().getGet();
-            }
-        }
-
-        assertEquals(found.getPrice()*realBoughtQuantity, convenienceService.purchase(productName, amount));
-        assertEquals(q- amount, found.getQuantity());
-    }
-
-    @Test
-    void 투쁠투_프로모션_재고_초과_정가구매_Y_테스트() {
-        System.setIn(new ByteArrayInputStream("Y".getBytes()));
-
-        String productName = "효택도시락2";
-        int amount = 8;
-
-        Product found = products.getProduct(productName, true);
-        Product found2 = products.getProduct(productName, false);
-
-        assertEquals(found.getPrice()*6, convenienceService.purchase(productName, amount));
-        assertEquals(0, found.getQuantity());
-        assertEquals(1, found2.getQuantity());
-    }
-
-    @Test
-    void 원쁠삼십_23개구매_테스트() {
-        String productName = "효택워터";
-        int amount = 23;
-
-        Product found = products.getProduct(productName, true);
-        int q = found.getQuantity();
-
-        assertEquals(found.getPrice(), convenienceService.purchase(productName, amount));
-        assertEquals(q-(found.getPromotion().getGet()+1), found.getQuantity());
-    }
-
-    @Test
-    void 원쁠삼십_프로모션_재고_초과_정가구매_Y_테스트() {
-        System.setIn(new ByteArrayInputStream("Y".getBytes()));
-
-        String productName = "효택소세지";
-        int amount = 33;
-
-        Product found = products.getProduct(productName, true);
-        int q = found.getQuantity();
-        Product found2 = products.getProduct(productName, false);
-        int q2 = found2.getQuantity();
-
-        assertEquals(found.getPrice()*(amount-q+1), convenienceService.purchase(productName, amount));
-        assertEquals(0, found.getQuantity());
-        assertEquals(q2-(amount-q), found2.getQuantity());
-    }
-
-    @Test
-    void 원쁠삼십_프로모션_재고_초과_정가구매_N_테스트() {
-        System.setIn(new ByteArrayInputStream("N".getBytes()));
-
-        String productName = "효택소세지";
-        int amount = 33;
-
-        Product found = products.getProduct(productName, true);
-
-        assertEquals(found.getPrice(), convenienceService.purchase(productName, amount));
-        assertEquals(0, found.getQuantity());
+        assertEquals(found.getPrice()*2, convenienceService.purchase(productName, amount));
+        assertEquals(q - amount, found.getQuantity());
     }
 }
--- src/test/java/store/service/PromotionServiceTest.java
+++ src/test/java/store/service/PromotionServiceTest.java
@@ -1,2 +1,77 @@
-package store.service;public class PromotionServiceTest {
+package store.service;
+
+import org.junit.jupiter.api.Test;
+import store.domain.Product;
+import store.domain.Products;
+import store.domain.Promotions;
+import store.model.PurchaseResult;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PromotionServiceTest {
+    Products products = new Products(new Promotions());
+    PromotionService promotionService = new PromotionService(products);
+
+    public PromotionServiceTest() throws IOException {}
+
+    @Test
+    void 투쁠투_프로모션_재고_초과_정가구매_Y_테스트() {
+        System.setIn(new ByteArrayInputStream("Y".getBytes()));
+
+        String productName = "효택도시락2";
+        int amount = 8;
+
+        PurchaseResult purchaseResult = new PurchaseResult(amount);
+
+        Product found = products.getProduct(productName, true);
+
+        promotionService.promotionQuantityCheck(found, purchaseResult);
+
+        assertEquals(amount, purchaseResult.getAmount());
+    }
+
+    @Test
+    void 원쁠삼십_프로모션_재고_모자랄때_공짜물품_제공_동의_테스트() {
+        System.setIn(new ByteArrayInputStream("Y".getBytes()));
+
+        String productName = "효택소세지";
+        int amount = 10;
+
+        Product found = products.getProduct(productName, true);
+        PurchaseResult purchaseResult = new PurchaseResult(amount);
+
+        promotionService.askServeExtraProduct(purchaseResult, found);
+
+        assertEquals(found.getQuantity(), purchaseResult.getAmount());
+    }
+
+    @Test
+    void 투쁠원_공짜물품_제공_동의_테스트() {
+        System.setIn(new ByteArrayInputStream("Y".getBytes()));
+
+        String productName = "사이다";
+        int amount = 5;
+
+        Product found = products.getProduct(productName, true);
+        PurchaseResult purchaseResult = new PurchaseResult(amount);
+
+        promotionService.askServeExtraProduct(purchaseResult, found);
+        assertEquals(6, purchaseResult.getAmount());
+    }
+
+    @Test
+    void 투쁠원_유효하지_않은_공짜물품_제공_동의_테스트() {
+        String productName = "사이다";
+        int amount = 4;
+
+        Product found = products.getProduct(productName, true);
+        PurchaseResult purchaseResult = new PurchaseResult(amount);
+
+        promotionService.askServeExtraProduct(purchaseResult, found);
+
+        assertEquals(amount, purchaseResult.getAmount());
+    }
 }
