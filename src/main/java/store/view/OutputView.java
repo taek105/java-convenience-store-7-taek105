fc1792f5ee644b981583efeb9d6a92fbd6006b07
taek105 <ht5172@gmail.com>
2024-11-08 13:00:26 +0900

    refactor(OutputView): string formatting
    출력 포매팅을 적용했습니다

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -5,17 +5,28 @@ import store.domain.Products;
 import store.model.PurchaseResult;
 import store.model.Receipt;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class OutputView {
     public static void printWelcome() {
         System.out.println("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n");
     }
 
     public static void printProducts(Products products) {
+        NumberFormat formatter = NumberFormat.getNumberInstance(Locale.KOREA);
+
         for ( Product product : products.getProductsList() ) {
             StringBuilder sb = new StringBuilder("- ");
             sb.append(product.getName()).append(" ").
-                    append(product.getPrice()).append("원 ").
-                    append(product.getQuantity()).append("개 ");
+                    append(formatter.format(product.getPrice())).append("원 ");
+
+            if ( product.getQuantity() > 0 ) {
+                sb.append(product.getQuantity()).append("개 ");
+            }
+            else {
+                sb.append("재고 없음");
+            }
 
             if ( product.isPromotion() ) {
                 sb.append(product.getPromotion().getName());
@@ -26,29 +37,36 @@ public class OutputView {
 
     public static void printReceipt(Receipt receipt) {
         int totalAmount = 0;
-        StringBuilder sb = new StringBuilder("=============W 편의점===============\n");
-        sb.append("상품명 / 수량 / 금액\n");
-        for (PurchaseResult purchaseResult : receipt.getPurchaseResults() ) {
+        NumberFormat formatter = NumberFormat.getNumberInstance(Locale.KOREA);
+
+        StringBuilder sb = new StringBuilder("==============W 편의점================\n");
+        sb.append(String.format("%-10s\t%4s\t%10s\n", "상품명", "수량", "금액"));
+        for (PurchaseResult purchaseResult : receipt.getPurchaseResults()) {
             totalAmount += purchaseResult.getAmount();
-            sb.append(purchaseResult.getName()).append(" / ");
-            sb.append(purchaseResult.getAmount()).append(" / ");
-            sb.append(purchaseResult.getPromotedPrice()).append('\n');
+            sb.append(String.format("%-10s\t%4d\t%10s\n",
+                    purchaseResult.getName(),
+                    purchaseResult.getAmount(),
+                    formatter.format(purchaseResult.getPromotedPrice())));
         }
 
-        sb.append("=============증   정================\n");
-        for (PurchaseResult purchaseResult : receipt.getPurchaseResults() ) {
-            if ( purchaseResult.getExtraAmount() > 0 ) {
-                sb.append(purchaseResult.getName()).append(" / ");
-                sb.append(purchaseResult.getExtraAmount()).append('\n');
+        sb.append("=============증\t정===============\n");
+        for (PurchaseResult purchaseResult : receipt.getPurchaseResults()) {
+            if (purchaseResult.getExtraAmount() > 0) {
+                sb.append(String.format("%-10s\t%4d\n",
+                        purchaseResult.getName(),
+                        purchaseResult.getExtraAmount()));
             }
         }
 
         sb.append("====================================\n");
-        sb.append("총구매액 / ").append(totalAmount).append(" / ").append(receipt.getPromotedPrice()+receipt.getPromotionKeep()).append('\n');
-        sb.append("행사할인 / -").append(receipt.getPromotionKeep()).append('\n');
-        sb.append("멤버십할인 / -").append(receipt.getMembershipKeep()).append('\n');
-        sb.append("내실돈 / ").append(receipt.getPromotedPrice()-receipt.getMembershipKeep()).append('\n');
-
+        sb.append(String.format("%-10s\t%4d\t%10s\n", "총구매액", totalAmount,
+                formatter.format(receipt.getPromotedPrice() + receipt.getPromotionKeep())));
+        sb.append(String.format("%-10s\t\t-%10s\n", "행사할인",
+                formatter.format(receipt.getPromotionKeep())));
+        sb.append(String.format("%-10s\t\t-%10s\n", "멤버십할인",
+                formatter.format(receipt.getMembershipKeep())));
+        sb.append(String.format("%-10s\t\t %10s\n", "내실돈",
+                formatter.format(receipt.getPromotedPrice() - receipt.getMembershipKeep())));
 
         System.out.println(sb);
     }
